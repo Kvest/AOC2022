e2c297a7318e5d8d3ba00d16f55cc942f90fc09b
Roman Bielokon <belokon.roman@gmail.com>
2022-12-03 00:41:50 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.toCalories().maxOrNull() ?: 0
+}
+
+fun part2(input: List<String>): Int {
+    return input
+        .toCalories()
+        .sorted()
+        .takeLast(3)
+        .sum()
+}
+
+private fun List<String>.toCalories(): List<Int> {
+    val result = mutableListOf<Int>()
+    var current = 0
+
+    this.forEach { row ->
+        if (row.isEmpty()) {
+            result.add(current)
+            current = 0
+        } else {
+            current += row.toInt()
+        }
+    }
+    result.add(current)
+
+    return result
 }
