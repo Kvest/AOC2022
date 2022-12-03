d502b450ea5ab8dce341382421f0d03874f01404
Roman Bielokon <belokon.roman@gmail.com>
2022-12-03 14:33:11 +0100

    Solve day 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,11 +9,11 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Int {
     return input.toCalories().maxOrNull() ?: 0
 }
 
-fun part2(input: List<String>): Int {
+private fun part2(input: List<String>): Int {
     return input
         .toCalories()
         .sorted()
---
+++ src/Day02.kt
@@ -0,0 +1,51 @@
+fun main() {
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return input.sumOf(String::toScore)
+}
+
+private fun part2(input: List<String>): Int {
+    return input
+        .map(String::correctMove)
+        .sumOf(String::toScore)
+}
+
+private val SCORES = mapOf(
+    "AX" to 4,
+    "AY" to 8,
+    "AZ" to 3,
+    "BX" to 1,
+    "BY" to 5,
+    "BZ" to 9,
+    "CX" to 7,
+    "CY" to 2,
+    "CZ" to 6,
+)
+
+private fun String.toScore(): Int {
+    return SCORES["${this[0]}${this[2]}"] ?: throw IllegalStateException("Unknown ${this[0]}${this[2]}")
+}
+
+private val CORRECTIONS = mapOf(
+    "AX" to "A Z",
+    "AY" to "A X",
+    "AZ" to "A Y",
+    "BX" to "B X",
+    "BY" to "B Y",
+    "BZ" to "B Z",
+    "CX" to "C Y",
+    "CY" to "C Z",
+    "CZ" to "C X",
+)
+
+private fun String.correctMove(): String {
+    return CORRECTIONS["${this[0]}${this[2]}"] ?: throw IllegalStateException("Unknown ${this[0]}${this[2]}")
+}
